106f6ca3c1332fda1614a932b13e5a485da6729d
dake-luo <35069119+dake-luo@users.noreply.github.com>
2020-11-08 17:05:23 +0800

    去掉多余注解

--- src/main/java/com/example/demo/aop/AutoLogAspect.java
+++ src/main/java/com/example/demo/aop/AutoLogAspect.java
@@ -9,7 +9,6 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
@@ -22,7 +21,6 @@ import java.lang.reflect.Method;
  */
 @Aspect
 @Component
-@EnableAspectJAutoProxy
 public class AutoLogAspect {
 
     /**
